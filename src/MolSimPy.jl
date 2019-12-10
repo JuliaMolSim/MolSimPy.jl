90fac3f7fc8255ccb6bc732a3eb4bede07e3a627
Sebastian Micluța-Câmpeanu <31181429+SebastianM-C@users.noreply.github.com>
2019-12-10 22:49:12 +0200

    Move neighbourlist back to ASE

--- src/MolSimPy.jl
+++ src/MolSimPy.jl
@@ -7,15 +7,4 @@ include("MatSciPy.jl")
 using .MatSciPy
 using ASE: ASEAtoms
 
-function neighbourlist(at::ASEAtoms, cutoff::Float64)::MatSciPy.NeighbourList
-   # TODO: also recompute if rcut is different !!!!!
-   # if no previous neighbourlist is available, compute a new one
-   # if !has_transient(at, (:nlist, cutoff)) || recompute
-   #    # this nlist will be destroyed as soon as positions change
-   #    set_transient!(at, (:nlist, cutoff), MatSciPy.NeighbourList(at, cutoff))
-   # end
-   # return get_transient(at, (:nlist, cutoff))
-   return MatSciPy.NeighbourList(at, cutoff)
-end
-
 end # module
